a3009db068ed3676b16a84be421e5f192d8eb144
Belousov Maksim <lib.bmw@gmail.com>
2021-05-30 00:51:06 +0300

    Add cache for event_list API handler.

--- events-http-server/src/main.rs
+++ events-http-server/src/main.rs
@@ -36,7 +36,7 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .data(redis.clone())
             .data(json_config)
-            .app_data(cache.clone())
+            .data(cache.clone())
             .service(web::scope("/v1").configure(v1::routes))
     })
     .bind("127.0.0.1:8080")
